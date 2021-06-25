448fc4a1f45909a0bf830a634a3bdc98672f7a78
Kris Hopper <hopperdavis@gmail.com>
2021-06-25 15:05:19 -0700

    add HTML framework for results

--- src/main.js
+++ src/main.js
@@ -8,9 +8,10 @@ import SpaceTraveler from './../src/galactic-age.js';
 $(document).ready(function () {
   $("form#space-age").submit(function(event)  {
     event.preventDefault();
-    // const ageEarth = $("#current-age").parseInt();
-    // const lifeExpectancy = $("#life-expectancy").parseInt();
-    // let msFrizzle = new SpaceTraveler(ageEarth, lifeExpectancy);
-    // mercuryCalc(msFrizzle);
+    const ageEarth = parseInt($("#current-age").val());
+    const lifeExpectancy = parseInt($("#life-expectancy").val());
+    let msFrizzle = new SpaceTraveler(ageEarth, lifeExpectancy);
+    $("#ageEarth").text(ageEarth);
+    $("#lifeExpectancy").text(lifeExpectancy);
   });
 });
