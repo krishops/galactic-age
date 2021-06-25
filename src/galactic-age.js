f5342e7982d7f515e4752b8b6b346adf3d1885c8
Kris Hopper <hopperdavis@gmail.com>
2021-06-25 10:38:30 -0700

    pass fourth test

--- src/galactic-age.js
+++ src/galactic-age.js
@@ -18,6 +18,7 @@ export default class SpaceTraveler  {
       return mercuryLifeExpectancy;
   }
   mercuryLifeToLive() {
-    
+    let lifeleftMercury = this.mercuryLife() - this.mercuryCalc();
+      return lifeleftMercury;
   }
 }
